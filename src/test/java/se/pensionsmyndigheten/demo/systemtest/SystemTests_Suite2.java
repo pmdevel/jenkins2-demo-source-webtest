511db2665e58a633077fcbb63d146c568bac1a45
niah9119 <niclas.ahlstrand@gmail.com>
2017-03-20 23:06:24 +0100

    Splitted tests

---
+++ src/test/java/se/pensionsmyndigheten/demo/systemtest/SystemTests_Suite2.java
@@ -0,0 +1,36 @@
+package se.pensionsmyndigheten.demo.systemtest;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import se.pensionsmyndigheten.demo.WebAppTest;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import static junit.framework.Assert.assertTrue;
+
+public class SystemTests_Suite2 extends WebAppTest {
+
+
+
+
+    @BeforeClass
+    public static void init() throws Exception {
+        WebAppTest.init();
+    }
+
+    @Test
+    public void checkApplicationVersionIsCorrect() throws IOException {
+        super.checkApplicationVersionIsCorrect();
+    }
+
+    @Test
+    public void checkPageTitleIsCodeNite_PROFILE() throws IOException {
+        super.checkPageTitleIsCodeNite_PROFILE();
+    }
+
+    @Test
+    public void checkHelloMessageIs_HelloFrom_PROFILE() throws IOException {
+        super.checkHelloMessageIs_HelloFrom_PROFILE();
+    }
+}
